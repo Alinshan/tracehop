a5e32c0b407601bf919733241da1350a542c03ef
shaan.403 <86637047+Alinshan@users.noreply.github.com>
2025-08-12 21:32:49 +0530

    Create gui.ts

---
+++ src/gui.ts
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { preload: path.join(__dirname, "preload.js") }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.on("ready", createWindow);
